1dac9d6a5404da8083d4e4f907af99f463f84a34
juncheol08 <juncheol08@naver.com>
2023-07-06 17:34:26 +0900

    java17 start

---
+++ ch01/src/sec1/Example5.java
@@ -0,0 +1,36 @@
+package sec1;
+
+public class Example5 {
+    public static void main(String[] args) {
+
+        // char short int long double float
+        byte b1 = 104;
+//        byte b2 = -129; //해당 표현범위보다 작음(언더플로우)
+//        byte b3 = 128; //해당 표현범위보가 크면(오버플로우)
+
+        //2byte
+        char c1 = 'k';
+//        char c2 = -1;
+//        char c3 = 65536;
+//
+//        short s1 = 365;
+//        short s2 = -32769;
+//        short s3 = 32768;
+//
+//        int i1 = 1;
+//        int i2 = -2147483649;
+//        int i3 = 2147483649;
+//
+//        long l1 = 1;
+//        long l2 = -9223372036854775809L;
+//        long l3 = 9223372036854775808L;
+//
+//        float f1 =1;
+//        float f2 =1.4E-46f;
+//        float f3 =3.4028235E39f;
+//
+//        double d1 =1;
+//        double d2 =4.9E-325;
+//        double d3 =1.7976931348623157E309;
+    }
+}
