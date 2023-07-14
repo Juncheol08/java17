a25a48c98b619b87443fcc28f283eafdd588e36a
juncheol08 <juncheol08@naver.com>
2023-07-14 17:50:36 +0900

    230714 class & Overloading & Overriding ...

---
+++ ch05/src/sec2/Example4.java
@@ -0,0 +1,20 @@
+package sec2;
+
+import sec1.Qna;
+
+public class Example4 {
+    public static void main(String[] args) {
+
+        Qna q1 = new Qna();
+        q1.setTitle(Qna.DUMMY);
+        System.out.println(q1.getTitle()+1);
+
+        Qna q2 = new Qna();
+        q2.setTitle(Qna.DUMMY);
+        System.out.println(q2.getTitle()+1);
+
+        q1.method1(); //final static
+        q1.method2(); //x
+        q1.method3(); //static
+    }
+}
